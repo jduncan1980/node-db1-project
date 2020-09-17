f81ed118e053e45f4cbbf9b1da2ed07506d97866
Jason Duncan <jasonduncan@Jasons-MacBook-Pro.local>
2020-09-17 18:18:59 -0400

    switched DB to postgres

--- knexfile.js
+++ knexfile.js
@@ -1,38 +1,54 @@
-module.exports = {
-  development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./data/budget.db3",
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./data/migrations",
-      tableName: "knex_migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
-    },
-  },
+// Update with your config settings.
 
-  // update the following configuration to use PostgreSQL
-  production: {
-    client: "pg",
-    connection: {
-      host: "localhost", // if the server is not running on your computer provide the network address
-      database: "the name of the database to use in the postgres server", // <-- update
-      user: "a user that has access to the server and database", // <-- update
-      password: "the password for the user", // <-- update
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: "./data/migrations",
-      tableName: "knex_migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
-    },
-  },
+module.exports = {
+	development: {
+		client: 'sqlite3',
+		connection: {
+			filename: './data/budget.db3',
+		},
+	},
+
+	production: {
+		client: 'postgresql',
+		connection: 'postgresql://localhost/accounts',
+	},
 };
+
+// module.exports = {
+// 	development: {
+// 		client: 'sqlite3',
+// 		connection: {
+// 			filename: './data/budget.db3',
+// 		},
+// 		useNullAsDefault: true,
+// 		migrations: {
+// 			directory: './data/migrations',
+// 			tableName: 'knex_migrations',
+// 		},
+// 		seeds: {
+// 			directory: './data/seeds',
+// 		},
+// 	},
+
+// 	// update the following configuration to use PostgreSQL
+// 	production: {
+// 		client: 'pg',
+// 		connection: {
+// 			host: 'localhost', // if the server is not running on your computer provide the network address
+// 			database: 'accounts', // <-- update
+// 			user: 'jasonduncan', // <-- update
+// 			password: 'PassWord', // <-- update
+// 		},
+// 		pool: {
+// 			min: 2,
+// 			max: 10,
+// 		},
+// 		migrations: {
+// 			directory: './data/migrations',
+// 			tableName: 'knex_migrations',
+// 		},
+// 		seeds: {
+// 			directory: './data/seeds',
+// 		},
+// 	},
+// };
